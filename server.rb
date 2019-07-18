ace15cc8220e154cef71e54359289b83cc9d37cd
Jesse Bees <hi@toomanybees.com>
2019-07-17 23:13:09 -0400

    adjust styles, add some pride flags

--- server.rb
+++ server.rb
@@ -56,6 +56,52 @@ helpers do
 			"<a #{klass} #{href}>#{text}</a>".html_safe
 		end
 	end
+
+	FLAGS = [
+		{ name: "Rainbow",
+			colors:	%w{ #E40303 #FF8C00 #FFED00 #008026 #004DFF #750787 } },
+		{ name: "Philadelphia",
+			colors:	%w{ #000000 #784F17 #E40303 #FF8C00 #FFED00 #008026 #004DFF #750787 } },
+		{ name: "Gilbert Baker's 1978",
+			colors:	%w{ #FF69B6 #E70000 #FF8C00 #FFEF00 #00811F #00C1C1 #0044FF #760089 } },
+		{ name: "Bisexual",
+			colors:	%w{ #D9006F #744D98 #0033AB } },
+		{ name: "Pansexual",
+			colors:	%w{ #FF148C #FFDA00 #05AEFF } },
+		{ name: "Transgender",
+			colors:	%w{ #55CDFC #F7A8B8 #FFFFFF #F7A8B8 #55CDFC } },
+		{ name: "Asexual",
+			colors:	%w{ #000000 #A3A3A3 #FFFFFF #810082 } },
+		{ name: "Aromantic",
+			colors:	%w{ #3DA542 #A7D379 #FFFFFF #A9A9A9 #000000 } },
+		{ name: "Agender",
+			colors:	%w{ #000000 #B9B9B9 #FFFFFF #B8F483 #FFFFFF #B9B9B9 #000000 } },
+		{ name: "Genderqueer",
+			colors:	%w{ #B57EDC #FFFFFF #4A8123 } },
+		{ name: "Nonbinary",
+			colors:	%w{ #FFF430 #FFFFFF #9C59D1 #000000 } },
+		{ name: "Genderfluid",
+			colors:	%w{ #FB72BF #FEFDFE #D414DA #020202 #094EC5 } },
+		{ name: "Intersex",
+			colors: %i{ intersex } },
+	]
+
+	def random_heart_flag
+		flag = FLAGS.sample
+		label = "Heart emoji in #{flag[:name]} Pride colors"
+		hearts = flag[:colors]
+			.map do |color|
+				style = if color == :intersex
+					'background-image: radial-gradient(circle at center, #FFDA00, #FFDA00 20%, #7902AA 22%, #7902AA 35%, #FFDA00 37%, #FFDA00);'
+				else
+					"--color:#{color};"
+				end
+				"<span style=\"#{style}\" aria-hidden=\"true\">❤️</span>"
+			end
+			.join
+			.html_safe
+		%[<span class="flag" aria-label="#{label}" title="#{label}">#{hearts}</span>]
+	end
 end
 
 get "/" do
